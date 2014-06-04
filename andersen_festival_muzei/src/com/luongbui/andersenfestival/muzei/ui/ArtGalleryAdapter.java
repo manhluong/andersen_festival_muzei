b49f6345c6f405116c5e0bcf4e9f3eeed1aaac4d
luong <manh.luong@gmail.com>
2014-06-04 23:28:42 +0200

    better list of artists. added more portraits.

--- andersen_festival_muzei/src/com/luongbui/andersenfestival/muzei/ui/ArtGalleryAdapter.java
+++ andersen_festival_muzei/src/com/luongbui/andersenfestival/muzei/ui/ArtGalleryAdapter.java
@@ -8,9 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ArtGalleryAdapter extends ArrayAdapter<ArtPiece> {
+   
+   private static final int[] artistsIcons = {
+      R.drawable.giorgia_marras_icon,
+      R.drawable.bruno_zocca_icon,
+      R.drawable.luca_tagliafico_icon,
+      R.drawable.arianna_zuppello_icon,
+      R.drawable.daniele_castellano_icon,
+      R.drawable.letizia_iannaccone_icon,
+      R.drawable.jacopo_oliveri_icon,
+      R.drawable.stefano_tirasso_icon,
+      R.drawable.matilde_martinelli_icon,
+      R.drawable.olga_tranchini_icon,
+      R.drawable.anais_tonelli_icon,
+      R.drawable.silvia_venturi_icon,
+      R.drawable.francesca_consalvo_icon
+      };
    
    private final Context ctx;
    private final ArtPiece[] vals;
@@ -31,6 +48,8 @@ public class ArtGalleryAdapter extends ArrayAdapter<ArtPiece> {
       View rowView = inflater.inflate(R.layout.settings_list_item, parent, false);
       TextView textView = (TextView) rowView.findViewById(R.id.author_name);
       textView.setText(vals[position].getAuthor());
+      ImageView imageView = (ImageView) rowView.findViewById(R.id.author_icon);
+      imageView.setImageResource(artistsIcons[position]);
       return rowView;
       }
    }
